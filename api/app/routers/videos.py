13802ff179c24df458cc9ff1fe50bea219f24b9e
ytsalko <yariktb124@gmail.com>
2026-03-08 10:55:57 +0200

    Updated video upload router

--- api/app/routers/videos.py
+++ api/app/routers/videos.py
@@ -9,7 +9,6 @@ router = APIRouter()
 async def upload_video(file: UploadFile = File(...), project_id: str = "default_folder"):
     object_name = project_id + '/video.' + file.filename.split('.')[-1]
     if storage.upload_file(file, object_name):
-        return VideoUploadResponse(video_id="12345", message="Video uploaded successfully", upload_url=f"http://example.com/upload/{object_name}")
+        return VideoUploadResponse(project_id=project_id, message="Video uploaded successfully", upload_url=f"{storage.get_base_url()}/{object_name}")
     else:
-        return VideoUploadResponse(video_id="12345", message="Failed to upload video", upload_url="")
-    
+        return VideoUploadResponse(project_id=project_id, message="Failed to upload video", upload_url=f"{storage.get_base_url()}/{object_name}")
